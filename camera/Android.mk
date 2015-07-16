6e0445766f4329130f66bb0da47895127a38f78b
leonardo emanuele <manups4e@gmail.com>
2015-07-16 08:41:47 +0200

    ^^ fixed temp typo for camera wrapper

--- camera/Android.mk
+++ camera/Android.mk
@@ -8,7 +8,8 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
 LOCAL_C_INCLUDES += \
-    system/media/camera/include
+    system/media/camera/include \
+    device/lge/g3-common/include
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
